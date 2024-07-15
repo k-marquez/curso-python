890612c7ea01caaea12319fbf5be6561c144e0ee
Kevin <marquezberriosk@gmail.com>
2024-07-14 20:33:48 -0400

    variables definition and data types

---
+++ variables/variables.py
@@ -0,0 +1,10 @@
+myVar = 1 # tipo int
+myVar2 = None # sin tipo/valor asociado
+anotherVar = "Some text" # tipo str
+
+print(type(myVar))
+print(type(myVar2))
+print(type(anotherVar))
+
+print(isinstance(myVar, int))
+print(isinstance(myVar, str))
