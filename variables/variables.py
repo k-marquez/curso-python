ada10f734d36b3ce9183e503c5cb709436dae233
Kevin <marquezberriosk@gmail.com>
2024-07-16 23:33:43 -0400

    add set example

--- variables/variables.py
+++ variables/variables.py
@@ -13,13 +13,17 @@ text = "Some text" # type str
 customList = [1, 3, 4, 'dog', ['1', '2', True], None, {'key': 'value'}] # type list
 myTuple = ("hello", 1 , customList) # type tuple
 myDict = {"name": "Juan", "age": 18} # type dictionary
+customSet = {1, "Cat", (1, 2)} # type set, list and dict not supported
 
 print(type(number))
 print(type(withoutVal))
 print(type(text))
+print(type(myDict))
+print(type(customSet))
 print(customList)
 print(myTuple)
 print(myDict)
+print(customSet)
 
 print(isinstance(number, int))
 print(isinstance(number, str))
